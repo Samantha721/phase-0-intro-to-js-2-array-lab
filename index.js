d88a528389860d573bb993189cc188dc45621cab
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-04-06 21:04:23 -0700

    commit tree 38dbd5424e80bbed1f2088f373ed833b78ea6ee4

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+
+function appendCat (name) {
+    const newcats = cats.slice();
+    newcats.push (name);
+    return newcats;
+}
+
+function prependCat(name) {
+    const hellocats = cats.slice();
+    hellocats.unshift(name);
+    return hellocats;
+}
+
+function removeLastCat() {
+    const staycats = cats.slice();
+    staycats.pop();
+    return staycats;
+}
+
+function removeFirstCat () {
+    const byecats = cats.slice();
+    byecats.shift();
+    return byecats;
+}
+
